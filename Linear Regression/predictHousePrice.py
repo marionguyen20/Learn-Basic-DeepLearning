fc39707670ee604df191141d085e1fa240addea4
Mario Nguyễn <53334967+marionguyen20@users.noreply.github.com>
2021-02-19 14:17:52 +0700

    complete

---
+++ Linear Regression/predictHousePrice.py
@@ -0,0 +1,46 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt 
+
+#Read data
+data = pd.read_csv('Linear Regression\data_linear.csv').values
+N = data.shape[0] #Number of rows
+x = data[:,0].reshape(-1,1) #(-1) means unknow -> reshape with unknow rows (computer will calculate) and 1 column
+y = data[:,1].reshape(-1,1)
+plt.scatter(x,y)
+plt.xlabel("meter square")
+plt.ylabel("price")
+
+x = np.hstack((np.ones((N,1)), x))
+w = np.array([0.,1.]).reshape(-1,1)
+
+numOfIteration = 100
+cost = np.zeros ((numOfIteration, 1))
+learning_rate = 0.000001
+
+for i in range(1, numOfIteration):
+    r = np.dot(x,w) - y
+    cost[i] = 0.5*np.sum(r*r)
+    w[0] -= learning_rate*np.sum(r)
+    w[1] -= learning_rate*np.sum(np.multiply(r,x[:,1].reshape(-1,1)))
+    print(cost[i])
+
+predict = np.dot(x,w)
+plt.plot((x[0][1],x[N-1][1]),(predict[0],predict[N-1]),'r')
+x1 = 50
+y1 = w[0] + w[1]*x1
+print("Price of 50m^2 ",y1)
+plt.scatter(x1,y1,color = 'red')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
